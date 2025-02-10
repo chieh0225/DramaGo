c57d52daa62946987091dd003bb164f18b851526
chieh <secret12501250@gmail.com>
2025-02-10 18:51:12 +0800

    chore: 更新 vite.config.js base 路徑

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  // base 的寫法：
+  // base: '/Repository 的名稱/'
+  base: "/DramaGo/",
   plugins: [react()],
-})
+});
