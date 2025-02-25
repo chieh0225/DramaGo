264d4d74bc57e7a17cc542b7520b92696ab34f31
xiang <aa0955059329@gmail.com>
2025-02-25 15:09:46 +0800

    xinag footer&nav

--- src/pages/front/FrontHome.jsx
+++ src/pages/front/FrontHome.jsx
@@ -1,9 +1,9 @@
 const FrontHome = () => {
 
     return(<>
-        
-        <h2>我是前台首頁</h2>
-        
+        <main className="container pt-3">
+            <h2>我是前台首頁</h2>
+        </main> 
     </>)
 };
 
