16473206fb2fb128b55e5780fcb082becc976dc6
chieh <secret12501250@gmail.com>
2025-03-10 10:51:54 +0800

    feat: 新增重設密碼功能；改為使用 toastSlice 來顯示 Toast

--- src/pages/front/Profile.jsx
+++ src/pages/front/Profile.jsx
@@ -1,3 +1,5 @@
+// Todo 更換圖片功能
+
 import { NavLink, Outlet } from "react-router-dom";
 
 import avatar from "/images/avatar-3.png";
@@ -80,20 +82,44 @@ const Profile = () => {
             <div className="col-lg-4 mb-lg-0 mb-6">
               <div className="profile">
                 <div className="profile-info">
-                  <div className="flex-shrink-0">
-                    <img
-                      src={avatar}
-                      alt="正在COS的角色"
-                      width="200"
-                      height="200"
-                      className="rounded-circle object-fit-cover avatar-image"
-                    />
+                  <div className="profile-pic-container position-relative d-inline-block">
+                    <div className="flex-shrink-0">
+                      <img
+                        src={avatar}
+                        alt="正在COS的角色"
+                        width="200"
+                        height="200"
+                        className="rounded-circle object-fit-cover profile-pic"
+                      />
+                    </div>
+                    <a
+                      href=""
+                      className="change-btn rounded-circle position-absolute bg-white
+                  d-flex justify-content-center align-items-center"
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                      >
+                        <title>點擊更換個人圖片~</title>
+                        <g id="refresh_3_fill" fill="none">
+                          <path d="M24 0v24H0V0zM12.594 23.258l-.012.002-.071.035-.02.004-.014-.004-.071-.036c-.01-.003-.019 0-.024.006l-.004.01-.017.428.005.02.01.013.104.074.015.004.012-.004.104-.074.012-.016.004-.017-.017-.427c-.002-.01-.009-.017-.016-.018m.264-.113-.014.002-.184.093-.01.01-.003.011.018.43.005.012.008.008.201.092c.012.004.023 0 .029-.008l.004-.014-.034-.614c-.003-.012-.01-.02-.02-.022m-.715.002a.023.023 0 0 0-.027.006l-.006.014-.034.614c0 .012.007.02.017.024l.015-.002.201-.093.01-.008.003-.011.018-.43-.003-.012-.01-.01z" />
+                          <path
+                            fill="#FF8A20FF"
+                            d="M20 9.5a1.5 1.5 0 0 1 1.5 1.5 8.5 8.5 0 0 1-8.5 8.5h-2.382a1.5 1.5 0 0 1-2.179 2.06l-2.494-2.494a1.495 1.495 0 0 1-.445-1.052v-.028c.003-.371.142-.71.368-.97l.071-.077 2.5-2.5a1.5 1.5 0 0 1 2.18 2.061H13a5.5 5.5 0 0 0 5.5-5.5A1.5 1.5 0 0 1 20 9.5m-4.44-7.06 2.5 2.5a1.5 1.5 0 0 1 0 2.12l-2.5 2.5a1.5 1.5 0 0 1-2.178-2.06H11A5.5 5.5 0 0 0 5.5 13a1.5 1.5 0 1 1-3 0A8.5 8.5 0 0 1 11 4.5h2.382a1.5 1.5 0 0 1 2.179-2.06Z"
+                          />
+                        </g>
+                      </svg>
+                    </a>
                   </div>
+
                   <p className="fs-c mt-11 mb-3">
                     我正在COS  薛芳菲 (墨雨雲間)
                   </p>
-                  <p className="fs-3 fw-bold mb-3">于小姐</p>
-                  <p className="fs-6 mb-11">沙發馬鈴薯</p>
+                  <p className="fs-3 fw-bold mb-3">樂樂</p>
+                  <p className="fs-6 mb-11">愛追劇的樂</p>
                 </div>
                 <div className="profile-nav">
                   <ul className="nav nav-pills flex-column gap-5">
